e97fb8cec3e0bbf6783ae669f21ccd8ccdd6cdd5
Jorge Apaza <yochiwarez@gmail.com>
2019-10-21 16:59:18 -0500

    ..

--- .history/bin/main_20191021181732.rb
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env ruby
-# frozen_string_literal: true
-class Board
-    initialize 
-    puts "__|__|__"
-    puts "__|__|__"
-    puts "  |  |  "
--- .history/bin/main_20191021181741.rb
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env ruby
-# frozen_string_literal: true
-
-    puts "__|__|__"
-    puts "__|__|__"
-    puts "  |  |  "
