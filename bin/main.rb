926e8b23ebed0395691e907630fdb43a99dadf1a
Jorge Apaza <yochiwarez@gmail.com>
2019-10-21 10:45:35 -0500

    added player basic class

--- bin/main.rb
+++ bin/main.rb
@@ -1,16 +1,50 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
-class Board
-    initialize
 
-    arr = []
-    puts "__|__|__"
-    puts "__|__|__"
-    puts "  |  |  "
-    end
+class Board
+
+  @player1
+  @player2
+
+  def initialize(player1, player2)
+    @player1 = player1
+    @player2 = player2
+  end
+
+  def getPlaterNames
+    puts "insert player1's name"
+    name = gets.chomp
+    @player1.setName(name)
+  end
+
+  def printName
+    puts @player1.getName 
+  end
+
+  def draw
+    puts '   |   |   '
+    puts '-----------'
+    puts '   |   |   '
+    puts '-----------'
+    puts '   |   |   '
+  end
 end
 
 class Player
+  @name
+  @move
+  def setName(name)
+    @name = name
+  end
+
+  def getName
     @name
-    @move
+  end
 end
+
+mplayer1 = Player.new
+mplayer2 = Player.new
+myBoard = Board.new(mplayer1, mplayer2)
+
+myBoard.getPlaterNames
+
